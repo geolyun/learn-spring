b173228cfa3f3f454f3199ef7c66ccac244527f7
CutyTeddyBear <geolyun@gmail.com>
2023-08-18 16:26:00 +0900

    8/18

--- learn-jpa-and-hibernate/src/main/java/com/CutyTeddyBear/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/CutyTeddyBear/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.CutyTeddyBear.springboot.learnjpaandhibernate.course;
 
 import com.CutyTeddyBear.springboot.learnjpaandhibernate.course.Course;
 import com.CutyTeddyBear.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.CutyTeddyBear.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,17 +13,27 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     // @Autowired
     // private CourseJdbcRepository repository;
 
+    // @Autowired
+    // private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Now!", "geolyun"));
-        repository.insert(new Course(2, "Learn Azure Now!", "geolyun"));
-        repository.insert(new Course(3, "Learn JS Now!", "geolyun"));
+        repository.save(new Course(1, "Learn AWS Now!", "geolyun"));
+        repository.save(new Course(2, "Learn Azure Now!", "geolyun"));
+        repository.save(new Course(3, "Learn JS Now!", "geolyun"));
 
-        repository.deleteById(1);
+        repository.deleteById(1l);
 
-        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(2l));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("geolyun"));
+
+        System.out.println(repository.findByName("Learn JS Now!"));
     }
 }
