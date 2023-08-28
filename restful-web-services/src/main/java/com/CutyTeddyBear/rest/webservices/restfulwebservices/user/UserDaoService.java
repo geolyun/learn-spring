b50d704875b0d08d3d63415efcf4fff534729be8
CutyTeddyBear <geolyun@gmail.com>
2023-08-29 08:41:30 +0900

    8/29

--- restful-web-services/src/main/java/com/CutyTeddyBear/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/CutyTeddyBear/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -13,16 +14,32 @@ public class UserDaoService {
     private static List<User> users = new ArrayList<>();
     // 현재는 정적 리스트가 비어 있다. 이걸 여러 사용자로 초기화한다.
 
+    private static int usersCount = 0;
+
     static {
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Jin", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Jin", LocalDate.now().minusYears(20)));
     }
 
     public List<User> findAll() {
         return users;
     }
-    //public User save(User user) {
-    //public User findOne(int id) {
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
 
 }
---
+++ restful-web-services/src/main/java/com/CutyTeddyBear/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,60 @@
+package com.CutyTeddyBear.rest.webservices.restfulwebservices.user;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    // 정보는 UserDaoService에서 얻어올 예정(UserDaoService는 Spring이 관리)
+    //GET /users
+    @GetMapping("/users")
+    // 모든 사용자의 정보를 조회하는 REST API
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    // 특정 사용자의 정보를 조회하는 REST API
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if(user==null)
+            throw new UserNotFoundException("id:"+id);
+
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
+
+    //POST /users
+    @PostMapping("/users")
+    // @Valid 사용하면 바인딩이 수행될 때 객체에 정의된 유효성 검증이 자동으로 수행
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+
+        // /users/4 => /users /{id},   user.getID
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}") // 현재 요청의 URL에 추가하고 싶은 건 path("/{id}")이다
+                .buildAndExpand(savedUser.getClass()) // 이 변수는 생성된 사용자의 아이디로 바꿔주고 싶다
+                .toUri(); // 그걸 URI로 변환
+
+        // location - /users/4
+        return ResponseEntity.created(location).build();
+    }
+
+}
