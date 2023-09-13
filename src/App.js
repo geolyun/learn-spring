2a9422e4665ce8f23a63a7ac3075467d22b51e34
CutyTeddyBear <geolyun@gmail.com>
2023-09-14 08:47:30 +0900

    9/14

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import TodoApp from './components/todo/TodoApp'
+
+function App() {
+  return (
+    <div className='App'>
+      <TodoApp />
+    </div>
+  )
+}
+
+// function PlayingWithProps(properties) {
+  
+//   console.log(properties)
+//   console.log(properties.property1)
+//   console.log(properties.property2)
+
+//   return (
+//     <div></div>
+//   )
+// }
+
+export default App;
---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LogoutComponent from './LogoutComponent'
+import HeaderComponent from './HeaderComponent'
+import ListTodosComponent from './ListTodosComponent'
+import ErrorComponent from './ErrorComponent'
+import WelcomeComponent from './WelcomeComponent'
+import LoginComponent from './LoginComponent'
+
+import './TodoApp.css'
+
+export default function TodoApp() {
+    return (
+        <div className="TodoApp">            
+            <BrowserRouter>
+                <HeaderComponent />
+                <Routes>
+                    <Route path='/' element={<LoginComponent />} />
+                    <Route path='/login' element={<LoginComponent />} />
+                    <Route path='/welcome/:username' element={<WelcomeComponent />} />
+                    <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='/logout' element={<LogoutComponent />} />
+
+                    <Route path='*' element={<ErrorComponent />} />
+
+                </Routes>
+            </BrowserRouter>        
+        </div>
+    )
+}
