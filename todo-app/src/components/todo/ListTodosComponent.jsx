4062e21236583f5dd0ed8694817f8e4d275884ae
CutyTeddyBear <geolyun@gmail.com>
2023-09-21 23:34:17 +0900

    9/21

--- todo-app/src/components/todo/ListTodosComponent.jsx
+++ todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,25 +1,63 @@
+import { useEffect, useState } from "react";
+import { deleteTodoApi, retrieveAllTodosForUsernameApi } from "./api/TodoApiService";
+
 function ListTodosComponent() {
 
     const today = new Date();
+    
     const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
 
-    const todos = [
-                    {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
-                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
-                    {id: 3, description: 'Learn Docker', done: false, targetDate:targetDate},
-                ]
+    const [todos, setTodos] = useState([])
+
+    const [message, setMessage] = useState(null)
+
+    // const todos = [
+    //                 // {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+    //                 // {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+    //                 // {id: 3, description: 'Learn Docker', done: false, targetDate:targetDate},
+    //             ]
+
+    //useEffect - 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 실행할 수 있도록 하는 Hook
+
+    useEffect ( () => refreshTodos(), [])
+
+    function refreshTodos() {
+    
+        retrieveAllTodosForUsernameApi('geolyun')
+        .then(response => { 
+            setTodos(response.data)
+        }
+        
+        )
+        
+    }
+
+    function deleteTodo(id) {
+        deleteTodoApi('geolyun', id)
+        .then(
+            () => {
+                setMessage(`Delete of todo with ${id} successful`)
+                refreshTodos()
+            }
+        )
+        .catch(error => console.log(error))
+    }
 
     return (
         <div className='container'>
             <h1>Things You Want To Do!</h1>
+
+            {message && <div className="alert alert-warning">{message}</div>}
+            
+            
             <div>
                 <table className='table'>
                     <thead>
                             <tr>
-                                <td>ID</td>
-                                <td>Description</td>
-                                <td>Is Done?</td>
-                                <td>Target Date</td>
+                                <th>Description</th>
+                                <th>Is Done?</th>
+                                <th>Target Date</th>
+                                <th>Delete</th>
                             </tr>
                     </thead>
                     <tbody>
@@ -30,7 +68,10 @@ function ListTodosComponent() {
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toDateString()}</td>
+                                    {/* <td>{todo.targetDate.toDateString()}</td> */}
+                                    <td>{todo.targetDate.toString()}</td>
+                                    <td> <button className="btn btn-warning" 
+                                                    onClick={() => deleteTodo(todo.id)}>Delete</button></td>
                                 </tr>
                             )
                         )
---
+++ todo-app/src/components/todo/api/TodoApiService.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiClient = axios.create(
+    {
+        baseURL: 'http://localhost:8080'
+    }
+)
+    
+export const retrieveAllTodosForUsernameApi
+    = (username) => apiClient.get(`/users/${username}/todos`)
+
+export const deleteTodoApi
+    = (username, id) => apiClient.delete(`/users/${username}/todos/${id}`)
+
+
+// javascript에서 backtick(`) 문자를 사용하여 문자열을 표현한것을 템플릿 리터럴이라고 한다.
+// 이렇게 사용하면, 두가지 기능이 있는데 (1)줄바꿈을 쉽게 할수 있고, (2)문자열 내부에 표현식을 포함할 수 있게 된다.
