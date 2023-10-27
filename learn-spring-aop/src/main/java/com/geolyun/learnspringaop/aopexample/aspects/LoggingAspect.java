c145a56e44dcc403a0eb4857da4f66b281dfbc42
CutyTeddyBear <geolyun@gmail.com>
2023-10-27 23:53:00 +0900

    10/27

---
+++ learn-spring-aop/src/main/java/com/geolyun/learnspringaop/aopexample/aspects/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.geolyun.learnspringaop.aopexample.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class LoggingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    //Pointcut - When?
+    // execution(* PACKAGE.*.*(..))
+    @Before("execution(* com.geolyun.learnspringaop.aopexample.business.*.*(..))")
+    public void logMethodCall(JoinPoint joinPoint) {
+        logger.info("Before Aspect - {} is called with arguments: {}"
+                , joinPoint, joinPoint.getArgs()); //WHAT
+    }
+
+}
---
+++ learn-spring-aop/src/main/java/com/geolyun/learnspringaop/aopexample/business/BusinessService1.java
@@ -0,0 +1,26 @@
+package com.geolyun.learnspringaop.aopexample.business;
+
+import com.geolyun.learnspringaop.aopexample.data.DataService1;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class BusinessService1 {
+
+    private DataService1 dataService1;
+
+    public BusinessService1(DataService1 dataService1) {
+        this.dataService1 = dataService1;
+    }
+
+    public int calculateMax() {
+        int[] data = dataService1.retrieveData();
+        return Arrays.stream(data).max().orElse(0);
+    }
+
+    // Business Logic
+    // Data
+
+}
